8407313d00b78c12e6dfe359e1126612fd483608
Sree&Ash-Mon <sreeash1235@gmail.com>
2026-02-21 14:12:49 +0530

    Create prog2.java

---
+++ 02/prog2.java
@@ -0,0 +1,26 @@
+// q Write a Java program to find the Fibonacci series up to N ?
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms (N): ");
+        int n = sc.nextInt();
+
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        sc.close();
+    }
+}
