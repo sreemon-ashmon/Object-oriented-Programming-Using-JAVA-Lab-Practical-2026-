601a3505f806b56db0d262bddf749bb875f61ac5
Sree&Ash-Mon <sreeash1235@gmail.com>
2026-02-27 13:38:57 +0530

    Create prog13.java

---
+++ 13/prog13.java
@@ -0,0 +1,21 @@
+// Q) Write a Java program to check whether a string is palindrome or not.
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if (str.equalsIgnoreCase(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
