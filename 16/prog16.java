02e84895f0ded33a4644a0494ec455926bf89288
Sree&Ash-Mon <sreeash1235@gmail.com>
2026-03-02 13:40:15 +0530

    Update prog16.java

--- 16/prog16.java
+++ 16/prog16.java
@@ -1,4 +1,7 @@
-// Q) Create a package named Music and declare a class Song.
+// Q ) Create a package named music and define a class Song inside it.
+// The Song class should have instance variables title, artist, and duration, and methods play() to play the song, 
+// and displayDetails() to display the song's details. Create another class Music in a different package and 
+// use the Song class to play a song and display its details.
 
 //--------------------------------mypack[Folder]--------------------------------------//
 // File: music/Song.java
