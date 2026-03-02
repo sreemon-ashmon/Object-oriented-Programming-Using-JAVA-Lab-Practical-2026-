6001f9ec6acf6c4377ab11259fce4d1f3b186c6a
Sree&Ash-Mon <sreeash1235@gmail.com>
2026-03-02 13:38:34 +0530

    Update prog16.java

--- 16/prog16.java
+++ 16/prog16.java
@@ -1,21 +1,41 @@
 // Q) Create a package named Music and declare a class Song.
 
 //--------------------------------mypack[Folder]--------------------------------------//
-// FILE 1 song.java :
-package Music;
+// File: music/Song.java
+package music;
 
 public class Song {
+    private String title;
+    private String artist;
+    private double duration; 
+
+
+    public Song(String title, String artist, double duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
     public void play() {
-        System.out.println("Song is playing...");
+        System.out.println("Now playing: " + title + " by " + artist);
+    }
+
+    public void displayDetails() {
+        System.out.println("Song Title : " + title);
+        System.out.println("Artist     : " + artist);
+        System.out.println("Duration   : " + duration + " minutes");
     }
 }
+// File: player/Music.java
+package player;
 
-//FILE 2 Main.java :
-import Music.Song;
+import music.Song;
 
-public class Main {
+public class Music {
     public static void main(String[] args) {
-        Song s = new Song();
-        s.play();
+        Song song = new Song("Shape of You", "Ed Sheeran", 4.24);
+
+        song.displayDetails();
+        song.play();
     }
 }
